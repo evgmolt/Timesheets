0e888a8f9cd105b1dcb9af0163bc2e83cd7773ed
evgmolt <evg.molt@gmail.com>
2021-05-17 15:09:11 +0400

    Controller and repository Ok

---
+++ Timesheets/Timesheets/Controllers/PersonsController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Timesheets.Data.Implementations;
+using Timesheets.Data.Interfaces;
+using Timesheets.Models;
+using Timesheets.Models.Dto;
+
+namespace Timesheets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        private readonly ILogger<PersonsController> _logger;
+        private readonly IRepository _personsRepository;
+
+        public PersonsController(IRepository personsRepository, ILogger<PersonsController> logger)
+        {
+            _logger = logger;
+            _personsRepository = personsRepository;
+        }
+
+        /// <summary>
+        /// Получает данные человек по Id
+        /// </summary>
+        /// <param name="personId"></param>
+        /// <returns> Данные человека </returns>
+        [HttpGet("person/{personId}")]
+        public ActionResult GetPerson([FromRoute] int personId)
+        {
+            _logger.LogDebug("GetPerson");
+            var response = _personsRepository.GetPerson(personId);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Получает список людей с заданным именем
+        /// </summary>
+        /// <param name="personName"></param>
+        /// <returns></returns>
+        [HttpGet("personName/{personName}")]
+        public ActionResult GetPersonByName([FromRoute] string personName)
+        {
+            var response = _personsRepository.GetPersonsByName(personName);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Получает список людей в заданном диапазоне
+        /// </summary>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
+        [HttpGet("personsWithPagination")]
+        public ActionResult GetPersonWithPagination(int skip, int take)
+        {
+            var response = _personsRepository.GetPersonsWithPagination(skip, take);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Создает новый объект Person
+        /// </summary>
+        /// <param name="personRequest"></param>
+        /// <returns> id созданного объекта </returns>
+        [HttpPost("create")]
+        public ActionResult CreatePerson([FromBody] PersonRequest personRequest)
+        {
+            int id = _personsRepository.Create(personRequest);
+            return Ok(id);
+        }
+
+        /// <summary>
+        /// Модифицирует данные человека
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [HttpPut("update")]
+        public ActionResult UpdatePerson([FromBody] Person person)
+        {
+            _personsRepository.Update(person);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Удаляет данные человека
+        /// </summary>
+        /// <param name="personId"></param>
+        /// <returns></returns>
+        [HttpDelete("delete")]
+        public ActionResult DeletePerson(int personId)
+        {
+            _personsRepository.Delete(personId);
+            return Ok();
+        }
+    }
+}
---
+++ Timesheets/Timesheets/Data/Implementations/PersonsRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Timesheets.Data.Interfaces;
+using Timesheets.Models;
+using Timesheets.Models.Dto;
+
+namespace Timesheets.Data.Implementations
+{
+    public class PersonsRepository : IRepository
+    {
+        private readonly string _jsonFileName = "persons.json";
+
+        private List<Person> persons;
+
+        public PersonsRepository()
+        {
+            if (File.Exists(_jsonFileName))
+            {
+                var json = File.ReadAllText(_jsonFileName);
+                persons = JsonSerializer.Deserialize<List<Person>>(json);
+            }
+        }
+
+        private void UpdatePersonsFile()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var json = JsonSerializer.Serialize(persons, options);
+            File.WriteAllText(_jsonFileName, json);
+        }
+
+        public int Create(PersonRequest personRequest)
+        {
+            int maxId = persons.Max(p => p.Id);
+            maxId++;
+            var person = new Person
+            {
+                Id = maxId,
+                FirstName = personRequest.FirstName,
+                LastName = personRequest.LastName,
+                Email = personRequest.Email,
+                Company = personRequest.Company,
+                Age = personRequest.Age
+            };
+            persons.Add(person);
+            UpdatePersonsFile();
+            return person.Id;
+        }
+
+        public void Delete(int id)
+        {
+            Person personForDelete = persons.Single(p => p.Id == id);
+            persons.Remove(personForDelete);
+            UpdatePersonsFile();
+        }
+
+        public Person GetPerson(int id)
+        {
+            return persons.Single(p => p.Id == id);
+        }
+
+        public IEnumerable<Person> GetPersonsByName(string name)
+        {
+            return persons.Where(p => p.FirstName == name || p.LastName == name);
+        }
+
+        public IEnumerable<Person> GetPersonsWithPagination(int skip, int take)
+        {
+            return persons.Where(p => p.Id > skip && p.Id < (skip + take + 1));
+        }
+
+        public void Update(Person person)
+        {
+            var personForUpdate = persons.Single(p => p.Id == person.Id);
+            persons.Remove(personForUpdate);
+            persons.Add(person);
+            UpdatePersonsFile();
+        }
+    }
+}
